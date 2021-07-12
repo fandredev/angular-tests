a759d0073ad979430f4114f159dde1579d819033
felipe <profissionalf.andre@gmail.com>
2021-07-12 20:41:35 -0300

    feat: add liked component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular testing';
+  likes = 0
+
+  like() : void {
+    this.likes++
+  }
 }
---
+++ src/app/shared/components/like-widget/like-widget.component.ts
@@ -0,0 +1,29 @@
+import { UniqueIdService } from './../../services/unique-id/unique-id.service';
+import { Component, Input, Output, EventEmitter, OnInit } from "@angular/core";
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons'
+
+
+@Component({
+  selector: 'app-like-widget',
+  templateUrl: './like-widget.component.html',
+  styleUrls: ['./like-widget.component.scss']
+})
+export class LikeWidgetComponent implements OnInit {
+  @Output() liked = new EventEmitter<void>()
+  @Input() likes = 0
+  @Input() id = null
+  fonts = {
+    faThumbsUp
+  }
+
+  constructor(private _uniqueIdService: UniqueIdService){}
+
+  ngOnInit() : void {
+    if(!this.id)
+      this.id = this._uniqueIdService.generateUniqueIdWithPrefix('like-widget')
+  }
+
+  like() : void {
+    this.liked.emit()
+  }
+}
